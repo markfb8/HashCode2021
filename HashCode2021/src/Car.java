7a0c1e1d165f7d7b12ba40d0cc98da2d33b9bda8
markf <markf@DESKTOP-CMM3OSM>
2021-02-25 19:10:08 +0100

    test commit

---
+++ HashCode2021/src/Car.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Car {
+	
+	private ArrayList<String> path;
+
+}
---
+++ HashCode2021/src/Intersection.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Intersection {
+	
+	private int id;
+	private ArrayList<String> in;
+	private ArrayList<String> out;
+	private ArrayList<TrafficLight> lights; //nomes tenen semafor les incoming
+
+}
